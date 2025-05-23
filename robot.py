f77098f63a6b1b5d0c8d6e3ab06f4f40c43c5737
Mike Silva <mike.a.silva@gmail.com>
2025-05-23 12:38:51 -0400

    Minor changes

--- robot.py
+++ robot.py
@@ -2,7 +2,7 @@ import commands2
 from robotcontainer import RobotContainer
 
 
-class MyRobot(commands2.TimedCommandRobot):
+class Robot(commands2.TimedCommandRobot):
     def robotInit(self):
         self.container = RobotContainer()
 
