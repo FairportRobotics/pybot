9ad42cc07ece14cc8e44dca1ab27991468828a09
Mike Silva <mike.a.silva@gmail.com>
2025-05-26 11:12:03 -0400

    Pulled in additional subsytems

--- robotcontainer.py
+++ robotcontainer.py
@@ -5,20 +5,23 @@ import subsystems
 
 class RobotContainer:
     def __init__(self):
+        self.drivetrain_type = constants.DRIVETRAIN_TYPE.lower()
+        if self.drivetrain_type not in ["tank", "swerve"]:
+            raise ValueError(f"Invalid drivetrain type: {self.drivetrain_type}")
         # Subsystems
+        self.climber = subsystems.Climber()
         self.controller = subsystems.Controller(
             controller_type=constants.CONTROLLER_TYPE
         )
-        drivetrain_type = constants.DRIVETRAIN_TYPE.lower()
-        if drivetrain_type == "tank":
+        if self.drivetrain_type == "tank":
             self.drive = subsystems.TankDrive()
-        elif drivetrain_type == "swerve":
+        elif self.drivetrain_type == "swerve":
             self.drive = subsystems.SwerveDrive()
-        # self.elevator = subsystems.Elevator()
-        # self.feeder = subsystems.Feeder()
-        # self.intake = subsystems.Intake()
+        self.elevator = subsystems.Elevator()
+        self.feeder = subsystems.Feeder()
+        self.intake = subsystems.Intake()
         self.network_tables = subsystems.NetworkTables()
-        # self.shooter = subsystems.Shooter()
+        self.shooter = subsystems.Shooter()
 
         # Configure button bindings
         self.configureButtonBindings()
--- subsystems/__init__.py
+++ subsystems/__init__.py
@@ -1,11 +1,23 @@
 from .controller import Controller
 from .networktables import NetworkTables
 from .tankdrive import TankDrive
-# TODO: Implement the rest of the subsystems
-# from .feeder import Feeder
-# from .elevator import Elevator
-# from .intake import Intake
-# from .shooter import Shooter
-# from .swervedrive import SwerveDrive
 
-__all__ = ["Controller", "NetworkTables", "TankDrive"]
+# TODO: Implement the rest of the subsystems
+from .climber import Climber
+from .elevator import Elevator
+from .feeder import Feeder
+from .intake import Intake
+from .shooter import Shooter
+from .swervedrive import SwerveDrive
+
+__all__ = [
+    "Climber",
+    "Controller",
+    "Elevator",
+    "Feeder",
+    "Intake",
+    "NetworkTables",
+    "Shooter",
+    "SwerveDrive",
+    "TankDrive",
+]
