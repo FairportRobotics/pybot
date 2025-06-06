3d5b920b8646d837978623504e9aab0530e2db13
Mike Silva <mike.a.silva@gmail.com>
2025-06-06 09:23:37 -0400

    Minor changes

--- components/__init__.py
+++ components/__init__.py
@@ -1,8 +1,5 @@
 from .controller import XboxController
-from .drivetrain import ArcadeDrive
+from .drivetrain import ArcadeDrive, TankDrive, SwerveDrive
 
 
-__all__ = [
-    "ArcadeDrive",
-    "XboxController"
-]
+__all__ = ["ArcadeDrive", "SwerveDrive", "TankDrive", "XboxController"]
