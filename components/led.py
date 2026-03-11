a9cf9f1ae29444b05a2ee4e3ae2ccc575db7ba92
Mike Silva <mike.a.silva@gmail.com>
2026-03-11 14:51:27 -0400

    Simplified LED component

--- components/led.py
+++ components/led.py
@@ -9,18 +9,20 @@ class LED:
         # Get the mode from the Network Tables
         led_mode = self.get_mode()
         # Change the LED mode based on the value
-        if led_mode == "blue":
-            self.blue()
-        elif led_mode == "green":
-            self.green()
-        elif led_mode == "red":
-            self.red()
-        elif led_mode == "rainbow":
+        if led_mode == "rainbow":
             self.rainbow()
+        elif led_mode in self.colors:
+            self.set_RGB(self.colors[led_mode])
         else:
             self.turn_off()
 
     def setup(self) -> None:
+        self.colors = {
+            "blue": (0, 0, 255),
+            "green": (0, 255, 0),
+            "red": (255, 0, 0),
+            "black": (0, 0, 0),
+        }
         self.led = AddressableLED(self.pwm_port)
         self.led.setLength(self.length)
         self.rainbow_first_pixel_hue = 0
@@ -35,24 +37,6 @@ class LED:
     # =========================================================================
     # CONTROL METHODS
     # =========================================================================
-
-    def blue(self):
-        """
-        Turn the LEDs blue
-        """
-        self.set_RGB(0, 0, 255)
-
-    def green(self):
-        """
-        Turn the LEDs geen
-        """
-        self.set_RGB(0, 255, 0)
-
-    def red(self):
-        """
-        Turn the LEDs red
-        """
-        self.set_RGB(255, 0, 0)
 
     def rainbow(self):
         """
@@ -79,10 +63,11 @@ class LED:
         """
         SmartDashboard.putString("LED Mode", mode)
 
-    def set_RGB(self, r, g, b):
+    def set_RGB(self, RGB):
         """
         Set the RGB values for the
         """
+        r, g, b = RGB
         for i in range(self.length):
             self.buffer[i].setRGB(r, g, b)
         self.led.setData(self.buffer)
@@ -92,7 +77,7 @@ class LED:
         """
         Turns the LEDs off.
         """
-        self.set_RGB(0, 0, 0)
+        self.set_RGB((0, 0, 0))
 
     # =========================================================================
     # INFORMATIONAL METHODS
