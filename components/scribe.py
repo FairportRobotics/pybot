df3fa26e635389bb95c95920daf69e441f8f4cb9
Mike Silva <mike.a.silva@gmail.com>
2026-03-13 09:10:44 -0400

    Initial commit of scribe.py

---
+++ components/scribe.py
@@ -0,0 +1,8 @@
+class Scribe:
+    def execute(self) -> None:
+        """Standard MagicBot loop. Updates internal state."""
+        pass
+
+    def setup(self) -> None:
+        """Called after injection but before the first loop."""
+        pass
