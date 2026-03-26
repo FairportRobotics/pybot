47e9bf6607ffc6730243349fec8540f5dd1c24d2
Mike Silva <mike.a.silva@gmail.com>
2026-03-26 09:43:10 -0400

    Extended accelerometer

--- components/accelerometer.py
+++ components/accelerometer.py
@@ -1,5 +1,6 @@
 import wpilib
 from magicbot import feedback
+import math
 import time
 
 
@@ -12,9 +13,13 @@ class RoboRioAccelerometer:
         self.y_accel = 0.0
         self.z_accel = 0.0
         self.last_time = time.monotonic()
-        self.G_TO_MPS2 = 9.80665 #9.81
+        self.G_TO_MPS2 = 9.80665  # 9.81
         self.velocity = {"x": 0.0, "y": 0.0, "z": 0.0}
         self.distance = {"x": 0.0, "y": 0.0, "z": 0.0}
+        self.pitch = 0.0
+        self.roll = 0.0
+        self.yaw = 0.0
+        self.quaternion = {"w": 0.0, "x": 0.0, "y": 0.0, "z": 0.0}
 
     def execute(self):
         now = time.monotonic()
@@ -46,6 +51,23 @@ class RoboRioAccelerometer:
             self.velocity[key] += acceleration[key] * dt
             # distance = distance + (velocity * dt)
             self.distance[key] += self.velocity[key] * dt
+
+        norm = math.sqrt(
+            acceleration["x"] ** 2 + acceleration["y"] ** 2 + acceleration["z"] ** 2
+        )
+        a_prime = {
+            "x": acceleration["x"] / norm,
+            "y": acceleration["y"] / norm,
+            "z": acceleration["z"] / norm,
+        }
+        self.pitch = math.atan2(
+            a_prime["x"], math.sqrt(a_prime["y"] ** 2 + a_prime["z"] ** 2)
+        )
+        self.roll = math.atan2(-a_prime["y"], -a_prime["z"])
+        self.quaternion["w"] = math.cos(self.roll / 2) * math.cos(self.pitch / 2)
+        self.quaternion["x"] = math.sin(self.roll / 2) * math.cos(self.pitch / 2)
+        self.quaternion["y"] = math.cos(self.roll / 2) * math.sin(self.pitch / 2)
+        self.quaternion["z"] = -math.sin(self.roll / 2) * math.sin(self.pitch / 2)
 
     # =========================================================================
     # CONTROL METHODS
@@ -95,3 +117,27 @@ class RoboRioAccelerometer:
     @feedback
     def velocity_z(self):
         return self.velocity["z"]
+
+    @feedback(key="pitch")
+    def the_pitch(self):
+        return self.pitch
+
+    @feedback(key="roll")
+    def the_roll(self):
+        return self.roll
+
+    @feedback
+    def quaternion_w(self):
+        return self.quaternion["w"]
+
+    @feedback
+    def quaternion_x(self):
+        return self.quaternion["x"]
+
+    @feedback
+    def quaternion_y(self):
+        return self.quaternion["y"]
+
+    @feedback
+    def quaternion_z(self):
+        return self.quaternion["z"]
